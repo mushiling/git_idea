b9fa293e98f5e2ff3c0a3a24b4f0b28dbda6b5a8
mushiling <85815138+mushiling@users.noreply.github.com>
2022-05-10 19:42:47 +0800

    pull commit

--- src/main/java/com/msl/git/gitTest.java
+++ src/main/java/com/msl/git/gitTest.java
@@ -9,5 +9,6 @@ public class gitTest {
         System.out.println("hello master test");
         System.out.println("hello hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
