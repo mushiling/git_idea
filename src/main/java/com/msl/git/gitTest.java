e825362c7817f1f272ac78bd1aefea87f5ef31e5
msl <msl@msl.com>
2022-05-10 19:17:25 +0800

    hot-fix commit2

--- src/main/java/com/msl/git/gitTest.java
+++ src/main/java/com/msl/git/gitTest.java
@@ -6,5 +6,6 @@ public class gitTest {
         System.out.println("hello git 2");
         System.out.println("hello git 3");
         System.out.println("hello hot-fix");
+        System.out.println("hello hot-fix test");
     }
 }
